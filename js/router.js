26bdc32936d74d5085e0100c161855f0a112ac8a
DESKTOP-JRGEBCQ\cheenlam <redsinsue@gmail.com>
2023-06-25 12:35:39 +0800

    程式完成

--- js/router.js
+++ js/router.js
@@ -9,7 +9,7 @@ Router.prototype.route = function(path, callback) {
 };
 Router.prototype.refresh = function() {
     // 路由清單
-    let routerList = ['', 'home','about','service/consultancy','service/project','service/critical','service/connected','protection/application','protection/origin','protection/dns','contact','privacy'];
+    let routerList = ['', 'home','problem','resources','about','contact','report'];
 
     //获取到相应的hash值
     let index = routerList.indexOf(location.hash.slice(2))
@@ -38,70 +38,36 @@ window.Router.init();
 
 // 路由呼叫路徑
 Router.route('/home', function() {
-    mainInclude('http://54.179.177.60/wp-content/themes/wk-finance/tpl/home.html');
-    $('#topMenu li').removeClass('--on');
+    mainInclude('tpl/home.html');
+    topmenu_highlight(1);
+});
+Router.route('/problem', function() {
+    mainInclude('tpl/problem.html');
+    topmenu_highlight(2);
+});
+Router.route('/resources', function() {
+    mainInclude('tpl/resources.html');
+    topmenu_highlight(3);
 });
 Router.route('/about', function() {
-    mainInclude('http://54.179.177.60/wp-content/themes/wk-finance/tpl/about.html');
-    $('#topMenu li').removeClass('--on');
-    $('#toAbout').addClass('--on');
+    mainInclude('tpl/about.html');
+    topmenu_highlight(4);
 });
-
-
-Router.route('/service/consultancy', function() {
-    mainInclude('http://54.179.177.60/wp-content/themes/wk-finance/tpl/consultancy.html');
-    chServiceMenu(0);
-});
-Router.route('/service/project', function() {
-    mainInclude('http://54.179.177.60/wp-content/themes/wk-finance/tpl/project.html');
-    chServiceMenu(1);
-});
-Router.route('/service/critical', function() {
-    mainInclude('http://54.179.177.60/wp-content/themes/wk-finance/tpl/critical.html');
-    chServiceMenu(2);
-});
-Router.route('/service/connected', function() {
-    mainInclude('http://54.179.177.60/wp-content/themes/wk-finance/tpl/connected.html');
-    chServiceMenu(3);
-});
-function chServiceMenu(num){
-    $('#topMenu li').removeClass('--on');
-    $('#toService').addClass('--on');
-    $(`#toService li:eq(${num})`).addClass('--on');
-}
-
-
-Router.route('/protection/application', function() {
-    mainInclude('http://54.179.177.60/wp-content/themes/wk-finance/tpl/application.html');
-    chProtectionMenu(0);
-});
-Router.route('/protection/origin', function() {
-    mainInclude('http://54.179.177.60/wp-content/themes/wk-finance/tpl/origin.html');
-    chProtectionMenu(1);
-});
-Router.route('/protection/dns', function() {
-    mainInclude('http://54.179.177.60/wp-content/themes/wk-finance/tpl/dns.html');
-    chProtectionMenu(2);
-});
-function chProtectionMenu(num){
-    $('#topMenu li').removeClass('--on');
-    $('#toProtection').addClass('--on');
-    $(`#toProtection li:eq(${num})`).addClass('--on');
-}
-
-
-
 Router.route('/contact', function() {
-    mainInclude('http://54.179.177.60/wp-content/themes/wk-finance/tpl/contact.html');
-    $('#topMenu li').removeClass('--on');
-    $('#toContact').addClass('--on');
+    mainInclude('tpl/contact.html');
+    topmenu_highlight(5);
+});
+Router.route('/report', function() {
+    mainInclude('tpl/report.html');
+    topmenu_highlight(6);
 });
 
-Router.route('/privacy', function() {
-    mainInclude('http://54.179.177.60/wp-content/themes/wk-finance/tpl/privacy.html');
-    $('#topMenu li').removeClass('--on');
-});
 
+// 變更topmenu亮起
+function topmenu_highlight(num){
+    $('#topMenu li').removeClass('--on');
+    $(`#topMenu li:eq(${num})`).addClass('--on');
+}
 
 function mainInclude(src) {
     $('#load').css('display','block');  
